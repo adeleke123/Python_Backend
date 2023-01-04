76fa2e13e0ec872d4fe20880107705edeb9dec11
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-01-04 04:52:38 +0100

    Task completed

--- Module_1/01-control_flow.py
+++ Module_1/01-control_flow.py
@@ -21,3 +21,22 @@ while i < 5:
 """
 This will list the numbers 0 through 4 on separate lines.
 """
+'''The break and continue statements are used to control the flow of a loop in Python.'''
+
+# The break statement is used to exit a loop prematurely. When the break statement is encountered inside a loop, it will cause the loop to terminate immediately, and the program will continue with the next statement after the loop. For example:
+
+for number in range(1, 10):
+    if number == 5:
+        break
+    print(number)
+
+'''In this example, the loop will iterate over the numbers 1 through 9, but it will stop and exit the loop when it reaches the number 5. The output of this code will be: 1, 2, 3, 4.'''
+
+# The continue statement is used to skip the remainder of the current iteration of a loop and move on to the next iteration. When the continue statement is encountered inside a loop, it will cause the loop to skip the rest of the code in the current iteration and move on to the next one. For example:
+
+for number in range(1, 10):
+    if number % 2 == 0:
+        continue
+    print(number)
+
+'''In this example, the loop will iterate over the numbers 1 through 9, but it will skip printing the even numbers (2, 4, 6, 8) and only print the odd numbers (1, 3, 5, 7, 9). The output of this code will be: 1, 3, 5, 7, 9.'''
