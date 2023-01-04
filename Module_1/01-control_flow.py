2307882c446c5b564bd98e69aa3ea90d2a6754d3
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-01-04 04:40:04 +0100

    loops

---
+++ Module_1/01-control_flow.py
@@ -0,0 +1,23 @@
+"""
+There are main two types of loop in python.They are
+1. for loops and
+2. while loops
+A for loop is used to iterate over a sequence (such as a list, tuple, or string) or other iterable object.
+A while loop is used to repeat a block of code as long as a certain condition is met.
+
+"""
+# Examples
+# The for loop will execute the code block for each item in the sequence.
+for number in [1, 2, 3, 4, 5]:
+    print(number)
+"""
+This will list the numbers 1 through 5 on separate lines.
+"""
+# The while loop will continue to execute the code block as long as the condition is True. For example:
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+"""
+This will list the numbers 0 through 4 on separate lines.
+"""
