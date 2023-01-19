6972de5cffdcfbf9b2da25c8c2c901b34ba308b9
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-01-19 16:12:24 +0100

    data types

---
+++ Module_1/09-data_types.py
@@ -0,0 +1,54 @@
+"""
+In Python, a datatype is a category of data that determines what kind of value a variable can hold and how it can be used. Here are some common datatypes in Python and examples of how to use them:
+"""
+# String: A string is a sequence of characters enclosed in single or double quotes. For example:
+
+name = "Muideen Adeleke"
+address = '123 Main St'
+print(name) # Muideen Adeleke
+print(address) # 123 Main St
+print(type(name)) # <class 'str'>
+
+# Integer: An integer is a whole number without a decimal point. For example:
+
+age = 25
+num_of_students = 30
+print(age) # 25
+print(num_of_students) # 30
+print(type(num_of_students)) # <class 'int'>
+
+# Float: A float is a decimal number. For example:
+
+price = 19.99
+distance = 3.14
+print(price) # 19.99
+print(distance) # 3.14
+print(type(price))
+
+# Boolean: A boolean value can be either True or False. For example:
+
+is_student = True
+is_valid = False
+print(is_student)
+print(is_valid)
+print(type(is_valid))
+
+# Dictionary: A dictionary is a collection of key-value pairs, where each key is unique. For example:
+
+student = {'name': 'John Smith', 'age': 25, 'address': '123 Main St'}
+
+print(student)
+print(type(student)
+
+# Set: A set is an unordered collection of unique elements. For example:
+
+Set = {1, 2, 3, 4}
+print(Set)
+print(type(Set))
+
+Tuple: A tuple is an ordered collection of elements. For example:
+student_info = ('John Smith', 25, '123 Main St')
+print(student_info)
+print(type(student_info))
+
+# It is important to use the appropriate datatype for your variables, as it can affect how your code works and how data is stored and processed.
