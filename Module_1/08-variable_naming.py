5e6223814dfafa8f2cd1293d0bb16ba95dc8681c
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-01-19 15:20:35 +0100

    variables

---
+++ Module_1/08-variable_naming.py
@@ -0,0 +1,27 @@
+"""
+In Python, variable naming conventions are guidelines for choosing the names of your variables. There are a few commonly used conventions:
+
+Variables should be in lowercase, with words separated by underscores. For example: my_variable, student_name
+
+Variables should be descriptive and meaningful. For example, instead of using "x" or "temp", use "student_name" or "temperature_in_fahrenheit".
+
+Variables should not start with a number. For example, "3rd_student" is not a valid variable name, but "third_student" is.
+
+Variables should not use any special characters except for underscores. For example, "student-name" is not a valid variable name, but "student_name" is.
+
+Variables should not use Python keywords as variable name. For example, "if" or "else" cannot be used as variable names.
+
+Example:
+
+"""
+#Good variable name
+student_name = "John"
+student_age = 25
+
+#Bad variable name
+3rd_student = "John"
+student-name = "John"
+if = "John"
+
+# It is important to follow these conventions in order to make your code readable and easy to understand for others.
+
