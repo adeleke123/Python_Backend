f13cab2cf3b75f69bce20440a95ddbf24473501a
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-01-19 13:31:18 +0100

    functions

--- Module_1/06-functions.py
+++ Module_1/06-functions.py
@@ -35,4 +35,21 @@ def greet(name, greeting="Hello"):
 greet("Adeleke")   # Output: Hello, Adeleke!
 greet("Adeleke", "Hi")   # Output: Hi, Adeleke!
 
+"""Functions in python also can take variable number of arguments, you can use *args for taking variable number of non-keyword arguments, and **kwargs for variable number of keyword argument."""
+
+def test_function(arg1, *args, **kwargs):
+    print(arg1)
+    print(args)
+    print(kwargs)
+test_function(1, 2, 3, 4, a=5, b=6)
+""" Functions in Python are a powerful feature, they allow you to organize your code and make it more readable and maintainable. They can take arguments, return values, have default values for arguments, and support variable number of arguments.
+
+You can also use lambda function, which is small anonymous function, with or without arguments and a single expression. This function can be used wherever function objects are required."""
+add = lambda x, y : x + y
+print(add(2,3)) 
+# output 5
+"""
+You can also use the built-in functions like map(), filter(), and reduce() to perform operations on lists, Tuples and other data structures with functions.
+
+In short, functions are a fundamental building block of Python programming, they help you to organize your code, make it more readable, and allow you to reuse your code multiple times throughout your program."""
 
