89bbfc35281640ed15be0dcdc4c36939d7ffad13
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-02 23:23:55 +0100

    constructor

--- Module_2/22-constructors.py
+++ Module_2/22-constructors.py
@@ -17,3 +17,28 @@ class Student:
 student = Student("John Doe", "12345")
 print(student.name) # Output: John Doe
 print(student.roll_number) # Output: 12345
+
+''' In this example, a Student class is defined with the __init__ method that takes two arguments: name and roll_number. When a Student object is created using student = Student("John Doe", "12345"), the __init__ method is automatically called and initializes the name and roll_number attributes of the student object.
+
+In Python, there are two types of constructors:
+
+Default Constructor: A default constructor is a constructor that is automatically created by Python if no __init__ method is defined in the class. The default constructor does not take any arguments and does not initialize any attributes.
+'''
+class Student:
+    pass
+
+student = Student()
+'''
+Parameterized Constructor: A parameterized constructor is a constructor that takes one or more arguments and initializes the attributes of the class. The __init__ method is used to define a parameterized constructor in Python.
+
+'''
+class Student:
+    def __init__(self, name, roll_number):
+        self.name = name
+        self.roll_number = roll_number
+
+student = Student("John Doe", "12345")
+'''
+Note: In Python, it is not mandatory to have a constructor in a class. If no constructor is defined, a default constructor is automatically created.
+
+'''
