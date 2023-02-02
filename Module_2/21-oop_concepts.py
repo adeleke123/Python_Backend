263908f7f8a324c0f90b28138a20fbc80b3d016a
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-02 22:58:56 +0100

    OOP concepts

---
+++ Module_2/21-oop_concepts.py
@@ -0,0 +1,4 @@
+"""
+Class inheritance is a feature in object-oriented programming languages like Python, which allows you to create a new class based on an existing class. The new class is called a derived class or a subclass and the existing class is called a base class or a superclass. The derived class inherits attributes and behaviors from the base class, and can also have its own unique attributes and behaviors.
+Here's a simple example to illustrate class inheritance in Python:
+"""
