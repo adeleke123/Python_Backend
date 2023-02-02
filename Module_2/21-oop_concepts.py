fd4fad57080aac9c1943e210c0ff53a3c72b7c95
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-02-02 23:05:39 +0100

    Update 21-oop_concepts.py

--- Module_2/21-oop_concepts.py
+++ Module_2/21-oop_concepts.py
@@ -1,5 +1,7 @@
 """
-Class inheritance is a feature in object-oriented programming languages like Python, which allows you to create a new class based on an existing class. The new class is called a derived class or a subclass and the existing class is called a base class or a superclass. The derived class inherits attributes and behaviors from the base class, and can also have its own unique attributes and behaviors.
+Class inheritance is a feature in object-oriented programming languages like Python, which allows you to create a new class based on an existing class. 
+The new class is called a derived class or a subclass and the existing class is called a base class or a superclass. 
+The derived class inherits attributes and behaviors from the base class, and can also have its own unique attributes and behaviors.
 Here's a simple example to illustrate class inheritance in Python:
 """
 class Animal:
@@ -21,5 +23,8 @@ class Dog(Animal):
 dog = Dog("Fido", "Labrador")
 dog.make_sound() # Output: Woof!
 """
-In this example, we first create a base class Animal with two attributes name and species and a method make_sound. Then we create a derived class Dog that inherits from Animal. The Dog class has its own __init__ method that calls the __init__ method of the Animal class and sets its own attribute breed. The Dog class also overrides the make_sound method with its own implementation.
+In this example, we first create a base class Animal with two attributes name and species and a method make_sound. 
+Then we create a derived class Dog that inherits from Animal. 
+The Dog class has its own __init__ method that calls the __init__ method of the Animal class and sets its own attribute breed. 
+The Dog class also overrides the make_sound method with its own implementation.
 """
