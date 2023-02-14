7eea6cd391512873675642da1278ec4dea3e62dc
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-14 22:56:08 +0100

    Modules

---
+++ Module_2/24-modules.py
@@ -0,0 +1,18 @@
+""" In Python, a module is a file containing Python definitions and statements. 
+
+The file name is the module name with the suffix ".py" appended to it.
+
+When you import a module, you can access its functions, classes, and variables in your program.
+
+Here's an example of a simple module named my_module.py:
+# my_module.py
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+pi = 3.14159
+
+In the example above, my_module.py contains two functions, add() and subtract(), and a variable pi. To use these functions and variable in another Python file, you can import the module as follows:
+"""
